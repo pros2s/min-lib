7fedd0c01ebaba2ac0bd19348c8dfb5464de684b
Alexandr <caiiiia_podoplelov@mail.ru>
2022-03-30 03:00:03 +0300

    colorpeacker and clear elements

--- src/js/modules/Castomizator.js
+++ src/js/modules/Castomizator.js
@@ -2,6 +2,7 @@ export default class Castomizator {
   constructor() {
     this.btnBlock = document.createElement('div');
     this.colorPeacker = document.createElement('input');
+    this.clear = document.createElement('div');
   }
 
   render() {
@@ -11,15 +12,20 @@ export default class Castomizator {
 
     panel.classList.add('panel');
     panel.append(this.btnBlock, this.colorPeacker);
+    this.clear.innerHTML = '&times';
+    this.clear.classList.add('clear');
 
     scaleInputSm.classList.add('scale_btn');
     scaleInputMe.classList.add('scale_btn');
     this.btnBlock.classList.add('scale');
+    this.colorPicker.classList.add('color');
 
     scaleInputSm.setAttribute('type', 'button');
     scaleInputMe.setAttribute('type', 'button');
     scaleInputSm.setAttribute('value', '1x');
     scaleInputMe.setAttribute('value', '1.5x');
+    this.colorPicker.setAttribute('type', 'color');
+    this.colorPicker.setAttribute('value', '#ffffff');
 
     this.btnBlock.append(scaleInputSm, scaleInputMe);
 
