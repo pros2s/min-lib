e98f62ced4f3431528508e7e81440a74417c996b
Alexandr <caiiiia_podoplelov@mail.ru>
2022-03-30 03:10:07 +0300

    font scale change method

--- src/js/modules/Castomizator.js
+++ src/js/modules/Castomizator.js
@@ -1,11 +1,44 @@
 export default class Castomizator {
   constructor() {
     this.btnBlock = document.createElement('div');
-    this.colorPeacker = document.createElement('input');
+    this.colorPicker = document.createElement('input');
     this.clear = document.createElement('div');
+    this.scale = localStorage.getItem('scale') || 1;
+
+    this.btnBlock.addEventListener('click', (e) => this.onScaleChange(e));
   }
 
+
+  onScaleChange(e) {
+    const body = document.querySelector('body');
+    if (e) this.scale = +e.target.value.replace(/x/g, '');
+
+    const recursy = (element) => {
+      element.childNodes.forEach(node => {
+
+        if (node.nodeName === '#text' && node.nodeValue.replace(/\s+/g, '').length > 0) {
+          if (!node.parentNode.getAttribute('data-fz')) {
+            let value = window.getComputedStyle(node.parentNode).fontSize;
+            node.parentNode.setAttribute('data-fz', +value.replace(/\D/g, ''));
+            node.parentNode.style.fontSize = node.parentNode.getAttribute('data-fz') * this.scale + 'px';
+          } else {
+            node.parentNode.style.fontSize = node.parentNode.getAttribute('data-fz') * this.scale + 'px';
+          }
+
+        } else {
+          recursy(node);
+        }
+      });
+    };
+
+    recursy(body);
+    localStorage.setItem('scale', this.scale);
+  }
+
+
   render() {
+    this.onScaleChange();
+
     const scaleInputSm = document.createElement('input'),
           scaleInputMe = document.createElement('input'),
           panel = document.createElement('div');
